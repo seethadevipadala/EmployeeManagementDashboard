c286bab342eb5180713230ec788b1f7225e553ac
seethadevipadala <seethadevipadala2000@gmail.com>
2022-10-04 11:37:53 +0530

    Import changes

--- src/components/empDashboard.js
+++ src/components/empDashboard.js
@@ -27,7 +27,7 @@ function EmpDashboard() {
   const openAddEmployeeModal = () => {
     setOpen(true);
   };
-  const closeAddEmployeeModal  = () => {
+  const closeAddEmployeeModal = () => {
     setOpen(false);
   };
 
@@ -56,12 +56,15 @@ function EmpDashboard() {
   };
 
   const onDeleteEmployee = () => {
+    // const newEmployees = employees.filter((employee) => {
+    //   return !selectedEmpIds.some((selectedId) => {
+    //     if (selectedId === employee.id) {
+    //       return employee;
+    //     }
+    //   });
+    // });
     const newEmployees = employees.filter((employee) => {
-      return !selectedEmpIds.some((selectedId) => {
-        if (selectedId === employee.id) {
-          return employee;
-        }
-      });
+      return selectedEmpIds.every((selectedId) => selectedId !== employee.id);
     });
     updateEmployees(newEmployees);
     closeConfirmDeleteModal();
@@ -72,7 +75,7 @@ function EmpDashboard() {
     closeConfirmDeleteAllModal();
   };
 
-  employees.forEach(employee => {
+  employees.forEach((employee) => {
     if (employee.gender === "male") {
       maleCount++;
     } else femaleCount++;
@@ -84,123 +87,116 @@ function EmpDashboard() {
   });
 
   return (
-    
-      <div className="app">
-        <div className="container">
-          <div className="button-div">
-            <h3 className="total">Total : {employees.length}</h3>
-          </div>
-
-          <div className="button-div">
-            <h3 className="male">Male : {maleCount}</h3>
-          </div>
-
-          <div className="button-div">
-            <h3 className="female">Female : {femaleCount}</h3>
-          </div>
-
-          <div className="button-div">
-            <h3 className="engineering">Engineering : {engineeringCount}</h3>
-          </div>
-
-          <div className="button-div">
-            <h3 className="backoffice">BackOffice : {backofficeCount} </h3>
-          </div>
+    <div className="app">
+      <div className="container">
+        <div className="button-div">
+          <h3 className="total">Total : {employees.length}</h3>
         </div>
-        <div className="adddiv">
-          <div className="add">
-            <button className="addbutton" onClick={openAddEmployeeModal}>
-              <Link to="/addemp">Add Employee</Link>
-              <Route path="/addemp">
-                open && (
-                  <AddEmployeeModal
-                    open={open}
-                    close={closeAddEmployeeModal }
-                    addEmployee={addEmployee}
-                  ></AddEmployeeModal>
-                )
-              </Route>
-            </button>
 
-            <div className="emplist">
-              <div className="deldiv">
-                <div>
-                  <input
-                    className="search"
-                    type="text"
-                    placeholder="Search..."
-                  ></input>
-                </div>
-                <div>
-                  <button
-                    name="button"
-                    disabled={!selectedEmpIds.length}
-                    id="button"
-                    className="delbutton"
-                    onClick={openConfirmDeleteModal}
-                  >
-                    <h3>Delete</h3>
-                  </button>
-                  {confirmDeleteModal && (
+        <div className="button-div">
+          <h3 className="male">Male : {maleCount}</h3>
+        </div>
+
+        <div className="button-div">
+          <h3 className="female">Female : {femaleCount}</h3>
+        </div>
+
+        <div className="button-div">
+          <h3 className="engineering">Engineering : {engineeringCount}</h3>
+        </div>
+
+        <div className="button-div">
+          <h3 className="backoffice">BackOffice : {backofficeCount} </h3>
+        </div>
+      </div>
+      <div className="adddiv">
+        <div className="add">
+          <button className="addbutton" onClick={openAddEmployeeModal}>
+            <Link to="/addemp">Add Employee</Link>
+            <Route path="/addemp">
+              open && (
+              <AddEmployeeModal
+                open={open}
+                close={closeAddEmployeeModal}
+                addEmployee={addEmployee}
+              ></AddEmployeeModal>
+              )
+            </Route>
+          </button>
+
+          <div className="emplist">
+            <div className="deldiv">
+              <div>
+                <input
+                  className="search"
+                  type="text"
+                  placeholder="Search..."
+                ></input>
+              </div>
+              <div>
+                <button
+                  name="button"
+                  disabled={!selectedEmpIds.length}
+                  id="button"
+                  className="delbutton"
+                  onClick={openConfirmDeleteModal}
+                >
+                  <h3>Delete</h3>
+                </button>
+                {confirmDeleteModal && (
+                  <div className="backshadow">
+                    <div className="modal">
+                      <h1>Delete</h1>
+                      <p>Are you sure you want to delete?</p>
+                      <button className="Yes" onClick={onDeleteEmployee}>
+                        Yes
+                      </button>
+                      <button className="No" onClick={closeConfirmDeleteModal}>
+                        No
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div>
+                <button
+                  className="delallbutton"
+                  onClick={openConfirmDeleteAllModal}
+                >
+                  <h3>Delete all</h3>
+                </button>
+
+                <>
+                  {confirmDeleteAllModal && (
                     <div className="backshadow">
                       <div className="modal">
-                        <h1>Delete</h1>
+                        <h1>Delete All the employees</h1>
                         <p>Are you sure you want to delete?</p>
-                        <button className="Yes" onClick={onDeleteEmployee}>
+                        <button className="Yes" onClick={onDeleteAllEmployee}>
                           Yes
                         </button>
                         <button
                           className="No"
-                          onClick={closeConfirmDeleteModal}
+                          onClick={closeConfirmDeleteAllModal}
                         >
                           No
                         </button>
                       </div>
                     </div>
                   )}
-                </div>
-                <div>
-                  <button
-                    className="delallbutton"
-                    onClick={openConfirmDeleteAllModal}
-                  >
-                    <h3>Delete all</h3>
-                  </button>
-
-                  <>
-                    {confirmDeleteAllModal && (
-                      <div className="backshadow">
-                        <div className="modal">
-                          <h1>Delete All the employees</h1>
-                          <p>Are you sure you want to delete?</p>
-                          <button className="Yes" onClick={onDeleteAllEmployee}>
-                            Yes
-                          </button>
-                          <button
-                            className="No"
-                            onClick={closeConfirmDeleteAllModal}
-                          >
-                            No
-                          </button>
-                        </div>
-                      </div>
-                    )}
-                  </>
-                </div>
+                </>
               </div>
-
-              
-                <EmployeeList
-                  employeeList={employees}
-                  onSelectEmployee={onSelectEmployee}
-                  selectedEmpIds={selectedEmpIds}
-                />
-              
             </div>
+
+            <EmployeeList
+              employeeList={employees}
+              onSelectEmployee={onSelectEmployee}
+              selectedEmpIds={selectedEmpIds}
+            />
           </div>
         </div>
       </div>
-  
+    </div>
   );
 }
 
--- src/components/employeeList.js
+++ src/components/employeeList.js
@@ -2,48 +2,45 @@ import React from "react";
 import "./employeeList.css";
 import pic from "../assets/pic.webp";
 import editpic from "../assets/edit_pic.png";
+
 const EmployeeList = ({ employeeList, onSelectEmployee, selectedEmpIds }) => {
   const handleChange = (event, employeeId) => {
     onSelectEmployee(event, employeeId);
   };
 
   return (
-    <>
-      <ul>
-        {employeeList.length > 0 ? (
-          employeeList.map((_emp, i) => (
-            <>
-              <div className="empdiv">
-                <div className="checkbox">
-                  <input
-                    className="checkBox"
-                    type="checkbox"
-                    onClick={(event) => {
-                      handleChange(event, _emp.id);
-                    }}
-                    value={i}
-                    checked={selectedEmpIds.includes(_emp.id)}
-                  />
-                </div>
-                <div className="image">
-                  <img className="profile" src={pic} alt="pic" />
-                </div>
-                <div className="name">
-                  <div>{_emp.name}</div>
-                  <div>({_emp.designation})</div>
-                </div>
+    <ul>
+      {employeeList.length > 0 ? (
+        employeeList.map((_emp, i) => (
+          <div className="empdiv">
+            <div className="checkbox">
+              <input
+                className="checkBox"
+                type="checkbox"
+                onClick={(event) => {
+                  handleChange(event, _emp.id);
+                }}
+                value={i}
+                checked={selectedEmpIds.includes(_emp.id)}
+              />
+            </div>
+            <div className="image">
+              <img className="profile" src={pic} alt="pic" />
+            </div>
+            <div className="name">
+              <div>{_emp.name}</div>
+              <div>({_emp.designation})</div>
+            </div>
 
-                <div className="edit">
-                  <img className="editimg" src={editpic} alt="pic" />
-                </div>
-              </div>
-            </>
-          ))
-        ) : (
-          <h3 className="nodata"> No Employee data</h3>
-        )}
-      </ul>
-    </>
+            <div className="edit">
+              <img className="editimg" src={editpic} alt="pic" />
+            </div>
+          </div>
+        ))
+      ) : (
+        <h3 className="nodata"> No Employee data</h3>
+      )}
+    </ul>
   );
 };
 export default EmployeeList;
